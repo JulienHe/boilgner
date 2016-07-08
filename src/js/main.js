7db122ca0f779a16e8df7f7b9030c64f3298f52e
Julien Henrotte <hoshibejul@gmail.com>
2016-07-09 00:13:34 +0200

    Travis

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,5 @@
 import {TapeMachine} from "./hello";
 
 const tp = new TapeMachine();
-tp.record("Hello... Hellooooo!!!");
+tp.record("Hey! So you are working with boilgner. I'm really happy to see you here. If you want to discuss about improvements of this boilerplate, you can send me an email to : hoshi.graphi@gmail.com");
 tp.play();
-// => Hello... Hellooooo!!! Helloooooo!!!!!
